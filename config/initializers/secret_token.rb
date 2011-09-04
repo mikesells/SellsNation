201ecdd960a1caa632a08208feff522609b9d17b
John Sells <jhsells@comcast.net>
2011-09-04 08:52:32 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SellsNation::Application.config.secret_token = '399de2338d697edb8fed69f7b4c9b59d104fb187f8e7aa0f69c673667d297774bbccccac75b069b531a78aa7fe4a8036fe5b267b26c3972debb4abe7f27d667a'
